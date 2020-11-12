00d421cfe622cfde646d1fa52df75cee55ca73c4
HokaHelal <eng.mah.ali@gmail.com>
2020-11-12 12:45:25 +0200

    set country name as required

--- Demo.Countries.Models/Country.cs
+++ Demo.Countries.Models/Country.cs
@@ -1,10 +1,12 @@
 ﻿using GenericRepository.Model;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Demo.Countries.Models
 {
     public class Country : Entity<int>
     {
+        [Required]
         public string Name { get; set; }
         public string Code { get; set; }
     }
---
+++ Demo.Countries.Repository/Migrations/20201112104030_SetCountryNameRequired.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Demo.Countries.Repository.Migrations
+{
+    public partial class SetCountryNameRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Countries",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Countries",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
